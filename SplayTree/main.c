7e94d1a48c326476dadafec909305f40778b553a
naleli <naleli415@gmail.com>
2026-04-17 11:47:40 -0400

    updated main tests, failed at insertion

--- SplayTree/main.c
+++ SplayTree/main.c
@@ -2,14 +2,33 @@
 #include <time.h>
 
 int main(){
+
     //initialize test
     printf("running initialization test...\n");
     Splay* tree1 = initialize();
     printf("initialization passed\n\n");
 
     //insertion test
+    printf("running insertion test...\n");
+
+    for(int i = 0; i < 1001; i++){
+        int rnum = rand() % 1001; //0...1000
+        insert(rnum, tree1);
+    }
+
+    printf("insertions passed\n\n");
 
     //retrieval test
+    printf("running retrieval test...\n");
+    int fish = 0; //retrieval var
+
+    for(int i = 0; i < 1001; i++){
+        int rnum = rand() % 1001; //0...1000
+        fish = get(rnum, tree1);
+    }
+
+    printf("retrievals passed\n\n");
+
     //destruction test
     printf("destroying tree...\n");
     destroyTree(tree1);
